8883fddf267f78d58b1e31deaa217488218d70d5
x-darkvanilla-x <dipesh.adelkar@gmail.com>
2023-09-15 16:47:57 +0530

    commit

---
+++ shop.js
@@ -0,0 +1,43 @@
+function hideall()
+{
+    document.getElementById("earring").style.display = "none";
+    document.getElementById("necklace").style.display = "none";
+    document.getElementById("dangler").style.display = "none";
+    document.getElementById("ring").style.display = "none";
+    document.getElementById("bracelet").style.display = "none";
+    document.getElementById("earring2").style.display = "none";
+    document.getElementById("necklace2").style.display = "none";
+    document.getElementById("dangler2").style.display = "none";
+    document.getElementById("ring2").style.display = "none";
+    document.getElementById("bracelet2").style.display = "none";
+}
+
+function show_earring()
+{
+    document.getElementById("earring").style.display = "block";
+    document.getElementById("earring2").style.display = "block";
+}
+
+function show_necklace()
+{
+    document.getElementById("necklace").style.display = "block";
+    document.getElementById("necklace2").style.display = "block";
+}
+
+function show_dangler()
+{
+    document.getElementById("dangler").style.display = "block";
+    document.getElementById("dangler2").style.display = "block";
+}
+
+function show_ring()
+{
+    document.getElementById("ring").style.display = "block";
+    document.getElementById("ring2").style.display = "block";
+}
+
+function show_bracelet()
+{
+    document.getElementById("bracelet").style.display = "block";
+    document.getElementById("bracelet2").style.display = "block";
+}
